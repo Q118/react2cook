90da5cfd09e6191333361f5cd1fd8ea4f6f9f868
Q118 <shelbyfish91@gmail.com>
2020-09-03 13:12:49 -0700

    functional buttons up and down

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,13 +4,29 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
 	//all code in here will be rendered into app component
+	constructor(props) {
+		super(props);
+
+		//state variable with a count property
+		this.state = {
+			count: props.initialCount,
+		};
+	}
+
 	render() {
 		return (
 			<div>
-				<button className="btn"> - </button>
-		<span>{this.props.initialCount}</span>
-				<button> + </button>
+				<button onClick={() => this.changeCount(-1) }> - </button>
+				<span>{this.state.count}</span>
+				<button onClick={() => this.changeCount(1) }> + </button>
 			</div>
 		);
 	}
+
+	changeCount(amount) {
+		this.setState({ count: this.state.count + amount })
+//take current count in our state and override it with the new count that we just calculated
+
+	}
+
 }
