144dd0170d84866772f6849bdbb718f35937533e
Q118 <shelbyfish91@gmail.com>
2020-09-03 16:08:33 -0700

    themeContext initialized

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import React from "react";
 import Counter from "./components/Counter";
 import CounterHooks from "./components/CounterHooks";
 
+export const ThemeContext = React.createContext();
+//consumer is allowing us to pass it a value 
+//essentially its providing a value to everything inside of it
+//and the consumer is just consuming that value 
+
+
 function App() {
 	return (
-		<>
+		<ThemeContext.Provider value={ {backgroundColor: theme} }>
 		Counter
 		<Counter initialCount={0} />
 		Counter Hooks
 		<CounterHooks initialCount={3} />
-		</>
+		</ThemeContext.Provider>
 	);
 }
 
