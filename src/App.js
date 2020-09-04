31354eb9b2c3b063168724f66d49c4778a837eb8
Q118 <shelbyfish91@gmail.com>
2020-09-03 18:49:09 -0700

    refacturing and initialized recipe components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 /** @format */
 
-import React, { useState } from "react";
-import Counter from "./components/Counter";
-import CounterHooks from "./components/CounterHooks";
-
-export const ThemeContext = React.createContext();
-//consumer is allowing us to pass it a value 
-//essentially its providing a value to everything inside of it
-//and the consumer is just consuming that value 
+import React from "react";
 
 
 function App() {
-	const [theme, setTheme] = useState("yellow")
-	return (
-		<ThemeContext.Provider value={ {backgroundColor: theme} }>
-		Counter
-		<Counter initialCount={0} />
-		Counter Hooks
-		<CounterHooks initialCount={3} />
-		<button onClick= {() => setTheme(prevTheme => {
-			return prevTheme === 'purple' ? 'yellow' : 'purple'
-		})}>Toggle Theme</button>
-		</ThemeContext.Provider>
-	);
+	
+	return null
 }
 
 export default App;
