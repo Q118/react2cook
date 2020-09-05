ba2940d33e61f271e8911b1ca06334d1ec17b14e
Q118 <shelbyfish91@gmail.com>
2020-09-04 17:00:51 -0700

    handleRecipeAdd setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 /** @format */
-import React from "react";
+import React, { useState } from "react";
 import RecipeList from './components/RecipeList';
 import './css/app.css'
+import { v4 as uuidv4 } from "uuid";
 
 
 function App() {
-	
+	const [recipes, setRecipes] = useState(sampleRecipes)
+
 	return (
 
 		<RecipeList recipes={sampleRecipes}/>
 
 	);
+	function handleRecipeAdd() {
+	const newRecipe = {
+		id: uuidv4(),
+		name: 'New',
+		servings: 1,
+		cookTime: '1:00',
+		instructions: 'Instr.',
+		ingredients: [
+			{ id: uuidv4(), name: "Name", amount: "1 Tbs" } 
+		]
+	}
+
+	//get all the recipes that are in our array right now
+	setRecipes([...recipes, newRecipe])
 }
+}
+
 
 
 const sampleRecipes = [ 
@@ -20,7 +38,7 @@ const sampleRecipes = [
 		name: "Plain Tofu",
 		servings: 3,
 		cookTime: "1:45",
-		instructions: "1. Put salt on tofu\n 2. Put tofu in oven\n 3. Eat tofu", 
+		instructions: "1. Put salt on tofu\n2. Put tofu in oven\n3. Eat tofu", 
 		ingredients: [
 			{
 				id: 1,
