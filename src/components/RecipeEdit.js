84fcf0f99b58979f154ab7147d137f638fde447b
Q118 <shelbyfish91@gmail.com>
2020-09-09 16:45:23 -0700

    edit ingredients layout

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -9,7 +9,15 @@ export default function RecipeEdit() {
             <div>
                 <label htmlFor="name">Name</label>
                 <input type="name" name="name" id="name" />
+                <label htmlFor="cookTime">Cook Time</label>
+                <input type="name" name="cookTime" id="cookTime" />
+                <label htmlFor="servings">Servings</label>
+                <input type="number" min="1" name="servings" id="servings" />
+                <label htmlFor="instructions">Instructions</label>
+                <textarea name="instructions" id="instructions"></textarea>
             </div>
+            <br />
+            <label>Ingredients</label>
         </div>
     )
 }
