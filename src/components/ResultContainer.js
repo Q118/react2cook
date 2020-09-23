d208487a653ab513d3cd9e1cfcd23cf1a421c6dc
Q118 <shelbyfish91@gmail.com>
2020-09-23 09:42:59 -0700

    layout of results page and hover effects

--- src/components/ResultContainer.js
+++ src/components/ResultContainer.js
@@ -8,13 +8,13 @@ import Button from "react-bootstrap/Button";
 const recipes = JSON.parse(localStorage.getItem("cookingWithReact.recipes"));
 
 export default function ResultContainer() {
-	const [searchTerm, setSearchTerm] = React.useState("Filter by Keyword");
+	const [searchTerm, setSearchTerm] = React.useState("");
 	const [searchResults, setSearchResults] = React.useState([]);
 	const handleChange = (event) => {
 		setSearchTerm(event.target.value);
 		console.log(recipes);
 	};
-	
+
 	React.useEffect(() => {
 		const results = recipes.filter((recipe) =>
 			recipe.name.toLowerCase().includes(searchTerm)
@@ -23,32 +23,37 @@ export default function ResultContainer() {
 	}, [searchTerm]);
 
 	return (
-		<div className="results">
-			<input
-				type="text"
-				placeholder="Filter by Keyword"
-				value={searchTerm}
-				onChange={handleChange}
-			/>
-			{searchResults.length ? (
-				searchResults.map((item) => (
-					<ResultCard
-						key={uuidv4()}
-                        name={item.name}
-                        servings={item.servings}
-						cookTime={item.cookTime}
-						instructions={item.instructions}
-						ingredients={item.ingredients}
-						// add link here to recipe-edit route route
-					/>
-				))
-			) : (
-				<p className="noResText">No Results to Display</p>
-			)}
-			<div>
-				<a variant="outline-success" className="search--button" href="./">
-					<Button>Back to CookBook</Button>
-				</a>
+		<div>
+			<div className="search--container">
+				<input
+					type="text"
+					className="search--input"
+					placeholder="Filter by Keyword"
+					value={searchTerm}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="results--container">
+				{searchResults.length ? (
+					searchResults.map((item) => (
+						<ResultCard
+							key={uuidv4()}
+							name={item.name}
+							servings={item.servings}
+							cookTime={item.cookTime}
+							instructions={item.instructions}
+							ingredients={item.ingredients}
+							// add link here to recipe-edit route route
+						/>
+					))
+				) : (
+					<p className="noResText">No Results to Display</p>
+				)}
+				<div>
+					<a variant="outline-success" className="search--button" href="./">
+						<Button>Back to CookBook</Button>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,17 +1,11 @@
 /** @format */
-
 import React from "react";
-import Form from "react-bootstrap/Form";
-
 import ResultContainer from "../components/ResultContainer";
 
 export default function SearchPage() {
-
-
-
 	return (
 		<div>
-				<ResultContainer />
+			<ResultContainer />
 		</div>
 	);
 }
