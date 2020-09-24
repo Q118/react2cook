c5f3f57cbbf442973da6e16f0a3920fdc44c621d
Q118 <shelbyfish91@gmail.com>
2020-09-24 14:53:07 -0700

    tooltips for code and user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,13 @@ import Nav from "react-bootstrap/Nav";
 import Search from "./Search";
 import Cook from "../Images/Cook.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faCodeBranch, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCode,
+	faCodeBranch,
+	faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "react-bootstrap/Button";
+import ReactTooltip from "react-tooltip";
 
 export default function Navbar() {
 	return (
@@ -22,20 +27,26 @@ export default function Navbar() {
 					<Search />
 				</div>
 				<div className="links__container">
-					<Link 
-					  href="https://github.com/Q118/react2cook"
-					  className="github--link"
-					>
+					<span data-tip data-for="github">
+					<Link
+						href="https://github.com/Q118/react2cook"
+						className="github--link">
 						<FontAwesomeIcon icon={faCode} />{" "}
 						<FontAwesomeIcon icon={faCodeBranch} />
 					</Link>
-					<Link 
-					  href="https://github.com/Q118/react2cook"
-					  className="my--link"
-					>
+					</span>
+					<ReactTooltip id="github"type="dark" effect="float" place="bottom">
+					<span>View Source Code</span>
+					</ReactTooltip>
+					<span data-tip data-for="user">
+					<Link href="https://github.com/Q118/react2cook" className="my--link">
 						<FontAwesomeIcon icon={faUser} />
 						{/* More from the Developer */}
 					</Link>
+					</span>
+					<ReactTooltip id="user"type="dark" effect="float" place="bottom">
+					<span>View Source Code</span>
+					</ReactTooltip>
 				</div>
 			</Nav>
 		</>
